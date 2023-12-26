128f3e0684f9aae994db3714886fdd0bb8fd454c
ramesh <ramesh.inexture@gmail.com>
2023-12-26 22:49:32 +0530

    feat/add posts, users, votes apis and refactore the code

--- app/oauth2.py
+++ app/oauth2.py
@@ -25,7 +25,6 @@ def create_access_token(data: dict):
 
 
 def verify_access_token(token: str, credentials_exception):
-
     try:
 
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
@@ -41,7 +40,8 @@ def verify_access_token(token: str, credentials_exception):
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                          detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+                                          detail=f"Could not validate credentials",
+                                          headers={"WWW-Authenticate": "Bearer"})
 
     token = verify_access_token(token, credentials_exception)
 
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from fastapi import status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from .. import models, schemas, utils
 from ..database import get_db
@@ -20,3 +20,12 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+
+@router.get("/", response_model=schemas.UserOut)
+def get_users(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} not found")
+
+    return user
